368c13bc7a9f38c25286d3b9e6c6c17e6a3914c2
Thomas Burns <thomasburns88@gmail.com>
2014-06-22 17:42:12 +1000

    Added MessageHandler func. This func now receives all packets and is responsible for writing them to each file.

--- daemon/SnotraServer.go
+++ daemon/SnotraServer.go
@@ -27,11 +27,11 @@ type Msg struct {
  *
  */
 
-func ClientReceiver(c net.Conn) {
+func ClientReceiver(c net.Conn, packets chan Msg) {
 	/*
 	 * Here we are connected to a specific client,
 	 * we wait until data is ready, then we unmarshal it into
-	 * a struct
+	 * a struct, and insert it into the chanel.
 	 */
 	buf := make([]byte, 4096)
 	for {
@@ -47,8 +47,21 @@ func ClientReceiver(c net.Conn) {
 		if err != nil {
 			continue
 		}
-		line := fmt.Sprintf("%s, %s, %s, %f\r\n", m.Module, m.Date, m.Parameter, m.Value)
+		packets <- m
+	}
+}
 
+func MessageHandler(packets chan Msg) {
+
+  /*
+   * Here we grab all incoming messages and sort them.
+   * This is a multiplexer of sorts.
+   */
+
+	for {
+
+		m := <-packets
+		line := fmt.Sprintf("%s, %s, %s, %f\r\n", m.Module, m.Date, m.Parameter, m.Value)
 		f, err := os.OpenFile(logDir+m.Module+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 		if err == nil {
 			f.Write([]byte(line))
@@ -66,12 +79,17 @@ func main() {
 		return
 	}
 
+	// Daemonise here....
+
+	incoming := make(chan Msg, 32)
+	go MessageHandler(incoming)
+
 	for {
 		fd, err := l.Accept()
 		if err != nil {
 			println("accept error", err.Error())
 			return
 		}
-		go ClientReceiver(fd)
+		go ClientReceiver(fd, incoming)
 	}
 }
